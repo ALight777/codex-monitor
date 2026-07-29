bd7a3545525066ed7691df2f58ad40b367696d6e
alight <alight@MacBook-Pro.local>
2026-07-29 15:53:54 +0800

    release: prepare v0.1.9

--- Sources/CodexNotch/AppInfo.swift
+++ Sources/CodexNotch/AppInfo.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum AppInfo {
-    static let version = "0.1.8"
+    static let version = "0.1.9"
 
     static var displayVersion: String {
         let bundleVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
